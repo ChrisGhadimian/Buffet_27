228f83da47ede2c27068e53b4255272f2fb689a1
ChrisGhadimian <vahik.ghadimian@gmail.com>
2026-09-17 22:02:21 +0000

    update

--- Java/000_Labs/009_math_methods/base_code/starter.java
+++ Java/000_Labs/009_math_methods/base_code/starter.java
@@ -10,5 +10,8 @@ class starter {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
 		System.out.print("I love to learn coding remotely."); 
+		int x = (int) (Math.random() * 100);
+		System.out.println(x);
+
 	}
 }
